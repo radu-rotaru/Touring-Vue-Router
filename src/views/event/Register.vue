540fff2163950ef67d2b84ac997712ed84f1c6ed
unknown <rotrast@gmail.com>
2022-02-24 01:23:19 +0200

    Completed L6

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,20 @@
 <template>
-  <p>Register for the event here</p>
+  <p>Regstration form here</p>
+  <button @click="register">Register Me!</button>
 </template>
-
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // If registration API call is successful
+      // Push back to the event details
+      // this.$router.push({
+      //   name: 'EventDetails',
+      //   params: { id: this.event.id }
+      // } )
+      this.$router.replace({ name: 'EventDetails', params: { id: 3 } })
+    }
+  }
 }
 </script>
