bae8ea528abd5ee3b0132713af36f7bb801c0ac1
unknown <rotrast@gmail.com>
2022-02-24 02:18:55 +0200

    Completed L8

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,11 +6,18 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
       // Call to API
       // If registered then redirect to event details
 
+    this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+      setTimeout(() => {  // After 3 seconds remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+      
       this.$router.push({
         name: 'EventDetails'
       })
