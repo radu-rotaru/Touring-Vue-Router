dfb0347249867d6145714521a4f2526b4c07731c
unknown <rotrast@gmail.com>
2022-02-24 12:43:10 +0200

    Completed L9

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -26,32 +26,49 @@
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-import { watchEffect } from 'vue'
+import NProgress from 'nprogress'
 
 export default {
   name: 'EventList',
   props: ['page'],
   components: {
-    EventCard
+    EventCard,
   },
   data() {
     return {
       events: null,
-      totalEvents: 0
+      totalEvents: 0,
     }
   },
-  created() {
-    watchEffect(() => {
-      this.events = null
-      EventService.getEvents(2, this.page)
-        .then(response => {
-          this.events = response.data
-          this.totalEvents = response.headers['x-total-count']
+  beforeRouteEnter(routeTo, routeFrom, next) {
+      NProgress.start()
+      return EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+        .then((response) => {
+          next(comp => {
+            comp.events = response.data
+            comp.totalEvents = response.headers['x-total-count']
+          })
         })
         .catch(() => {
-          this.$router.push({ name: 'NetworkError' })
+          next({ name: 'NetworkError' })
         })
-    })
+        .finally(() => {
+          NProgress.done()
+        })
+  },
+  beforeRouteUpdate(routeTo) {
+    NProgress.start()
+    return EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then(response => {
+        this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
+      })
+      .catch(() => {
+        return { name: 'NetworkError' }
+      })
+      .finally(() => {
+        NProgress.done()
+      })
   },
   computed: {
     hasNextPage() {
